ad3b48d6d7bf5211536f30f2dbd207524366d413
kplucinski <kamilplucinski@icloud.com>
2025-10-10 11:20:00 +0200

    abc

--- projekt/src/pracownicy.java
+++ projekt/src/pracownicy.java
@@ -1,2 +1,31 @@
 public class pracownicy {
+    private String imiePracownika ;
+    private String nazwiskoPracownika;
+
+
+
+    public pracownicy(String imiePracownika, String nazwiskoPracownika){
+        this.imiePracownika = imiePracownika;
+        this.nazwiskoPracownika = nazwiskoPracownika;
+
+    }
+    //imie
+    public String getImiePracownika(){
+        return imiePracownika;
+    }
+
+    public void setImiePracownika(String imiePracownika) {
+        this.imiePracownika = imiePracownika;
+    }
+
+    //nazwisko
+    public String getNazwiskoPracownika(){
+        return nazwiskoPracownika;
+    }
+
+    public void setNazwiskoPracownika(String nazwiskoPracownika) {
+        this.nazwiskoPracownika = nazwiskoPracownika;
+    }
+
+
 }
