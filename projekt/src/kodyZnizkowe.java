d547ca27aaaf1c47a4dd792279f79cf791d5bf7a
WagonToZiomal <96378061+WagonToZiomal@users.noreply.github.com>
2025-10-10 09:29:39 +0200

    klasy

--- projekt/src/kodyZnizkowe.java
+++ projekt/src/kodyZnizkowe.java
@@ -1,2 +1,19 @@
 public class kodyZnizkowe {
+    private String kodZnizkowy;
+    private double znizka;
+
+   public kodyZnizkowe() {
+
+   }
+
+    public String getKodZnizkowy() {
+        return kodZnizkowy;
+    }
+
+    public double getZnizka(){
+       return znizka;
+    }
+
+
+
 }
