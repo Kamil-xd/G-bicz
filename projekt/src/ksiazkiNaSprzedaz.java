7e0ded8af3b58e075331a79375cc734514f29407
kuswi <kuswikdominik@gmail.com>
2025-10-26 15:38:26 +0100

    a

--- projekt/src/ksiazkiNaSprzedaz.java
+++ projekt/src/ksiazkiNaSprzedaz.java
@@ -39,6 +39,18 @@ public class ksiazkiNaSprzedaz {
     //nazwa
 
 
+    public void setDostawca(dostawcy dostawca) {
+        this.dostawca = dostawca;
+    }
+
+    public void setNazwaProduktu(String nazwaProduktu) {
+        this.nazwaProduktu = nazwaProduktu;
+    }
+
+    public void setCenaKsiazki(double cenaKsiazki) {
+        this.cenaKsiazki = cenaKsiazki;
+    }
+
     @Override
     public String toString() {
         return ", nazwa" + nazwaProduktu + ", cenaKsiazki" + cenaKsiazki + ", dostawca" + dostawca;
